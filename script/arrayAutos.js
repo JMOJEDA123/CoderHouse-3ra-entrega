ce77a33316912c95eebe03674be158c3a90a3005
JMOR <[[200~151459786+JMOJEDA123@users.noreply.github.com~>
2024-05-01 11:21:58 -0300

    Primera version

--- script/arrayAutos.js
+++ script/arrayAutos.js
@@ -247,17 +247,18 @@ const contenedorPrincipal = document.getElementById("contenedor");  //Obtiene el
 const tituloMain = document.getElementById("titulo-main");  //Obtiene nombre del titulo del MAIN. OK
 let enlacesMarcas = document.getElementsByClassName("enlace");  //Obtiene los elementos HTML de los enlaces de consulta (NavTab)
 let botonDetalles = document.getElementsByClassName("boton-ver-detalles");//Obtiene los elementos boton detalle(Pendiente por realizar)
-let contenedorDetallesAutos = document.getElementById("contenedor-caracteristicas");//Obtiene el contenedor de informacion de detalles(Pendiente)
-let autosFavoritos =[];
+let contenedorDetallesAutos = document.getElementById("modal");//Obtiene el contenedor de informacion de detalles
+let botonCerrar = document.querySelector('.cerrar')
+let autosFavoritos = [];
+let autosDetalles =[];
 
+//********************** Seccion Principal que muestra card de autos segun criterio ************* */
 
 cargarAutos(arrayAutos);  //Al iniciar la pagina se cargan  TODOS los autos sin ningun tipo de filtro
-
 
 //Funcion para cargar dinamicamente TODOS los autos al contenedor principal del HTML
 function cargarAutos(autos) {
   contenedorPrincipal.innerHTML = ""; //Resetea el contenedor para iniciar con un array vacio
-  contenedorDetallesAutos.innerHTML = "";
   for (const auto of autos) { //Recorre todos los elementos del array que se pasa como parametro
     divElemento = document.createElement("div"); // Crea el elemento de referencia donde se insertara dinamicamente informacion
     divElemento.classList.add("contenedor-elementos");
@@ -278,13 +279,26 @@ function cargarAutos(autos) {
 
   //Asignar Evento boton favorito de tarjetas
   let corazonFavorito = document.getElementsByClassName("corazon-favorito");
+
   for (const btnFavorito of corazonFavorito) {
-    btnFavorito.addEventListener ('click', ()=>{
-    let autosFavoritos = autos.find (auto => auto.id == btnFavorito.id);
-    agregarFavoritos(autosFavoritos);
+    btnFavorito.addEventListener('click', () => {
+      let autosFavoritos = autos.find(auto => auto.id == btnFavorito.id);
+      agregarFavoritos(autosFavoritos);
+    })
+  }
+  
+  //Asignar Evento boton mostrar detalles
+  let botonDetalles = document.getElementsByClassName("boton-ver-detalles");
+  for (const detalle of botonDetalles) {
+    detalle.addEventListener('click', (e) => {
+      e.preventDefault();
+      let autosDetalles = autos.find(auto => auto.id == detalle.id);
+      mostrarDetalles(autosDetalles);
     })
   }
 }
+
+
 
 //Funcion para agregar autos a favoritos
 function agregarFavoritos(auto) {
@@ -293,26 +307,25 @@ function agregarFavoritos(auto) {
   localStorage.setItem('favoritos', jsonAutosFavoritos);
   cantFav = autosFavoritos.length;
   let numeroFavorito = document.getElementById("numero");
-  numeroFavorito.innerHTML =`${cantFav}`;
+  numeroFavorito.innerHTML = `${cantFav}`;
   const jsonNumeroFavorito = JSON.stringify(numeroFavorito.innerHTML);
   localStorage.setItem('numeroFavoritos', jsonNumeroFavorito);
 }
 
+
 //Asignacion de eventos a boton de favoritos GENERAL
-botonTodosFavoritos = document.querySelector (".logo-corazon");
-botonTodosFavoritos.addEventListener ("click", () => {
+botonTodosFavoritos = document.querySelector(".logo-corazon");
+
+botonTodosFavoritos.addEventListener("click", () => {
   cargarAutos(autosFavoritos);
   let tituloMain = document.getElementById("titulo-main");
   tituloMain.innerHTML = "Esta es tu lista de Autos Favoritos!"
 });
 
-function vaciarFavoritos(){
-  autosFavoritos=[];
-  numeroFavorito.innerHTML='';
+function vaciarFavoritos() {
+  autosFavoritos = [];
+  numeroFavorito.innerHTML = '';
 }
-
-
-
 
 
 //Bucle: for...of  para asignar un Manejador de Eventos a cada enlace (link) de MARCA de autos y filtar por esa propiedad
@@ -333,6 +346,63 @@ for (const link of enlacesMarcas) {//Recorre los elementos del array de objetos 
   })
 }
 
+
+//************************************** Inicio Seccion Mostrar Modal Detalles********************* */
+//Seccion Mostrar detalles
+function mostrarDetalles(autosDetalles) {
+
+  contenedorDetallesAutos.innerHTML = ""; //Resetea el contenedor para iniciar con un array vacio
+  contenedorDetallesAutos.innerHTML = ` 
+
+      <div class="modal-contenedor" id="contenedor-caracteristicas">
+        <div class="tabla-caracteristicas" id="tabla-caracteristicas">
+      
+          <h1>${autosDetalles.marca}   ${autosDetalles.modelo}  ${autosDetalles.agno}</h1>
+
+          <h3 class="titulo-caracteristica">Color</h3>
+          <h3 class="valor-caracteristica">${autosDetalles.color}</h3>
+
+          <h3 class="titulo-caracteristica">Puertas</h3>
+          <h3 class="valor-caracteristica">${autosDetalles.puertas}</h3>
+
+          <h3 class="titulo-caracteristica">Motor</h3>
+          <h3 class="valor-caracteristica">${autosDetalles.motor}</h3>
+
+          <h3 class="titulo-caracteristica">Transmision</h3>
+          <h3 class="valor-caracteristica">${autosDetalles.transmision}</h3>
+
+          <h3 class="titulo-caracteristica">Tipo Combustible</h3>
+          <h3 class="valor-caracteristica">${autosDetalles.tipoCombustible}</h3>
+
+          <h3 class="titulo-caracteristica">Kilometros</h3>
+          <h3 class="valor-caracteristica">${autosDetalles.kilometros}</h3>
+        </div>
+      
+        <div class="tabla-imagen" id="tabla-imagen">
+        <img src="${autosDetalles.urlFoto}" alt="${autosDetalles.modelo}" >
+        <i class="fa-regular fa-rectangle-xmark cerrar"></i>        
+        </div>
+
+      </div>
+    `;
+
+    contenedorDetallesAutos.style.opacity = "1"
+    contenedorDetallesAutos.style.pointerEvents = "unset"
+
+    // Agregar de evento al botón de cierre
+    botonCerrar = document.querySelector('.cerrar')
+    botonCerrar.addEventListener('click', ocultarModal);
+    
+}
+
+// Función para ocultar la ventana modal
+function ocultarModal(e) {
+  e.preventDefault();
+  contenedorDetallesAutos.style.opacity = "0"
+  contenedorDetallesAutos.style.pointerEvents = "none";
+}
+
+//************************************** Fin Seccion Mostrar Modal Detalles********************* */
 
 
 
@@ -423,6 +493,112 @@ botonSimulaCredito.addEventListener ("click", simularCredito );
 
 let botonLimpiarCampos = document.getElementById("boton-limpiar-campos");
 botonLimpiarCampos.addEventListener("click", limpiarCampos );
+
+
+
+
+
+
+
+
+
+// // ******************************** Inicio Seccion Simulacion de credito**********************************
+// //Funcion: simulacion de credito en cuotas
+// function simularCredito(evento) {
+
+//   evento.preventDefault()
+
+//   //Declaracion de Variables
+//   let tasaNomAnual;
+//   let cantAgnos;
+//   let numeroTotalCuotas;
+//   let tasaIntEquiv;
+//   let costoAutoElegido;
+//   let montoFinanciar;
+//   let anticipo;
+
+//   // Entrada de datos
+//   costoAutoElegido = parseInt(document.querySelector("#valor-auto").value);
+//   anticipo = parseInt(document.getElementById("monto-anticipo").value);
+
+//   //Monto a financiar para la adquisicion del auto
+//   montoFinanciar = costoAutoElegido - anticipo;
+
+//   // Entrada de datos para calculo del financiamiento
+//   tasaNomAnual = parseFloat(document.getElementById("tasa-finac").value);
+
+//   cantAgnos = parseFloat(document.getElementById("cant-agnos").value);
+
+//   //Calculo de numero total de cuotas a financiar
+//   numeroTotalCuotas = 12 * cantAgnos;
+
+//   //Calculo de tasa de interes equivalente
+//   tasaIntEquiv = tasaNomAnual / 12;
+
+//   //Funcion para calcular Tasa Efectiva Anual
+//   function calcularTea(tasaNomAnual, numeroTotalCuotas) {
+//     return ((Math.pow(1 + (tasaNomAnual / 100) / numeroTotalCuotas, numeroTotalCuotas) - 1) * 100);
+//   }
+
+//   tea = calcularTea(tasaNomAnual, numeroTotalCuotas);
+
+//   //Calculo de valor de la cuota de amortizacion
+//   let pmt = montoFinanciar * (tasaIntEquiv / 100) / (1 - Math.pow(1 + (tasaIntEquiv / 100), -numeroTotalCuotas));
+
+//   // Ciclo para calcular los pagos de todas las cuotas
+//   let saldoInicial = montoFinanciar;
+//   let arrayAmortizacion = [] //array vacio a llenar por iteracion
+
+//   for (let i = 0; i < numeroTotalCuotas; i++) {
+//     let intereses = saldoInicial * tasaIntEquiv / 100;
+//     let amortizacion = pmt - intereses;
+//     let saldoFinal = saldoInicial - amortizacion;
+
+//     arrayAmortizacion[i] = [[i], saldoInicial.toFixed(2), amortizacion.toFixed(2), intereses.toFixed(2), saldoFinal.toFixed(2)]
+
+//     saldoInicial = saldoFinal;
+//   }
+
+//   for (let i = 0; i < numeroTotalCuotas; i++) {
+//     let divTablaAmort = document.getElementById("cuerpo-tabla-amortizacion") //elemento del DOM de referencia
+//     let divNuevoAnidado = document.createElement("div");
+//     divNuevoAnidado.classList.add("tabla-amortizacion");
+//     divNuevoAnidado.setAttribute("id", "columnas-tabla-amortizacion");
+//     let cuotasGeneradas = `<p>${i + 1}</p><p>${arrayAmortizacion[i][1]}</p><p>${arrayAmortizacion[i][2]}</p><p>${arrayAmortizacion[i][3]}</p><p>${arrayAmortizacion[i][4]}</p>`;
+//     divNuevoAnidado.innerHTML = cuotasGeneradas
+
+//     divTablaAmort.append(divNuevoAnidado);
+
+//   }
+// }
+
+// //Funcion para limpiar campos de datos entrada
+// function limpiarCampos() {
+//   let divTablaAmort = document.getElementById("cuerpo-tabla-amortizacion") //elemento del DOM de referencia
+//   document.querySelector("#valor-auto").value = "";
+//   document.getElementById("monto-anticipo").value = "";
+//   document.getElementById("tasa-finac").value = "";
+//   document.getElementById("cant-agnos").value = "";
+//   divTablaAmort.innerHTML = ""
+// }
+
+// let botonSimulaCredito = document.getElementById("boton-simula-credito");
+// botonSimulaCredito.addEventListener("click", simularCredito);
+
+// let botonLimpiarCampos = document.getElementById("boton-limpiar-campos");
+// botonLimpiarCampos.addEventListener("click", limpiarCampos);
+
+// // ******************************** Fin Seccion Simulacion de credito**********************************
+
+
+
+
+
+
+
+
+
+
 
 
 
--- script/users.js
+++ script/users.js
@@ -1,57 +1,112 @@
-
+//Elementos HTML
 let botonSignUp = document.getElementById("sign-up");
 let botonSignIn = document.getElementById("sign-in");
 
 
+//Asignacion de eventos a Botones
+botonSignUp.addEventListener("click", registrar);
+botonSignIn.addEventListener("click", ingresar);
 
-function signUp(e) {
-  e.preventDefault();
+usuarioActivo = JSON.parse(sessionStorage.getItem("usuActivo"));
+let loginUsuario = document.querySelector(".login-usuario");
+loginUsuario.innerHTML = `Usuario: ${usuarioActivo} `;
 
-  // const AreaSaludo = document.getElementById('greetingArea');
-  const userName = prompt("Por favor ingrese su nombre de usuario: ");
 
-  // Comprueba si el usuario ya existe en el LocalStorage
-  const usuariosExistentes = localStorage.getItem('usuarios') || '[]';
-  const usuariosArray = JSON.parse(usuariosExistentes);
-
-  if (usuariosArray.includes(userName)) {
-    alert('Este nombre de usuario ya existe. Ingresa uno diferente.');
-    return;
-  }
-
-  // Guarda el usuario en el LocalStorage
-  usuariosArray.push(userName);
-  localStorage.setItem('usuarios', JSON.stringify(usuariosArray));
-
-  // Muestra un mensaje de saludo
-  alert(`¡Hola ${userName}! Bienvenido a Bitter&Sour Automotores.`);
-  //   greetingArea.innerHTML = mensajeSaludo;
+function actualizarUsuario (userName){
+  let loginUsuario = document.querySelector(".login-usuario");
+  usuarioActualizado = localStorage.getItem("usuActivo")
+  loginUsuario.innerHTML = `Usuario: ${userName} `;
+  
 }
 
 
+//************************************************************************************************* */
 
-
-function signIn(e) {
+//Inicio Funcion Signup (Registrarse)
+function registrar(e) {
   e.preventDefault();
 
-  const userName = prompt("Por favor ingreser su nombre de usuario: ");
+  (async () => {
+    const { value: userName } = await Swal.fire({
+      title: "Por Favor Ingresa un Nombre de Usuario",
+      position: "center",
+      input: "text",
+      inputPlaceholder: "Username",
+      inputValue: "",
+      showCancelButton: true,
+     toast:"true",
+    });
 
-  const usuariosExistentes = localStorage.getItem('usuarios') || '[]';
-  const usuariosArray = JSON.parse(usuariosExistentes);
+        // Comprobar si el usuario ya existe en el LocalStorage
+    const usuariosExistentes = localStorage.getItem('usuarios') || '[]';
+    const usuariosArray = JSON.parse(usuariosExistentes);
+  
+    if (usuariosArray.includes(userName)) {
+      // Muestra un mensaje de bienvenida
+      Swal.fire({
+        title: `Tu Nombre de Usuario Ya Existe!  Selecciona Otro Nombre!`,
+        icon: "error",
+      });
 
-  if (usuariosArray.includes(userName)) {
-    alert("Bienvenido nuevamente " + userName + "!!!");
-    return;
-  }
+    }else {
+    // Guarda el usuario en el LocalStorage
+      usuariosArray.push(userName);
+      Swal.fire({
+        title: `Gracias Por Registrate ${userName}!!!`,
+        icon: "success",
+      });
+    }
 
-  alert("Tu Usuario No existe! ... Te invitamos a suscribirte!");
-
+  })();
 }
+//Fin Funcion Signup (registrarse)
 
 
 
-botonSignUp.addEventListener("click", signUp);
-botonSignIn.addEventListener("click", signIn);
+//************************************************************************************************* */
+
+
+
+//Inicio Funcion INGRESAR
+function ingresar(e) {
+  e.preventDefault();
+
+  (async () => {
+    const { value: userName } = await Swal.fire({
+      title: "Por Favor Ingresa Tu Nombre de Usuario",
+      position: "center",
+      input: "text",
+      inputPlaceholder: "Username",
+      inputValue: "",
+      showCancelButton: true,
+    });
+
+    let usuariosExistentes = localStorage.getItem('usuarios') || '[]';
+    let usuariosArray = JSON.parse(usuariosExistentes);
+
+    // Comprobar si el usuario ya existe en el LocalStorage
+    if (usuariosArray.includes(userName)) {
+      sessionStorage.removeItem('usuActivo');
+      localStorage.setItem('usuarios', JSON.stringify(usuariosArray));
+      sessionStorage.setItem('usuActivo', JSON.stringify(userName));
+      actualizarUsuario(userName);
+      Swal.fire({
+        title: `Nos Alegra Verte Nuevamente ${userName}!!!`,
+        icon: "success",
+      });
+    } else {
+      // Muestra un mensaje de saludo
+      Swal.fire({
+        title: "Tu Nombre de Usuario No existe! Te invitamos a Suscribirte!",
+        icon: "error",
+      });
+    }
+
+  })();
+}
+//Fin Funcion Signin (Ingresar)
+
+
 
 
 
